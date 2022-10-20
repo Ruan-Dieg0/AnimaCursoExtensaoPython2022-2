3a6eca05a369e2b3334339f5ea421524eb2ff7ee
Ruan Diego Farias <69852934+Ruan-Dieg0@users.noreply.github.com>
2022-10-20 01:18:09 +0000

    Exibi strings e frases de formas diferentes

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ Meu primeiro projeto Python :D
 print() = Comando de saida
 Para guardar uma string (Frase): nome = "Ruan Diego" ou idade = 19
 Para exibir string nome/idade: print(nome) ou print(idade)
+Para exibir uma frase + string: print ("Meu nome e idade são: "+str(idade))
+   ou print (f"Meu nome e idade são {nome} {idade}\n")
+.
+str: conversão de inteiro para string
+
+Para exibir string e frase alternados: print("Meu nome é {} e tenho {} anos".format(nome, idade))
+
 
 '''
 
@@ -11,6 +18,8 @@ print ("Hello World")
 nome = "Ruan Diego"
 idade = 19
 print (nome, idade)
-
+print ("Meu nome e idade são: "+nome, idade)
+print (f"Meu nome e idade são: {nome} {idade}\n")
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
 
 
