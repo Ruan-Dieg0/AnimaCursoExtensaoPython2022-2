8ea101b0d9737e872c4cccdfeb3fbf5fe0c73da8
Ruan Diego Farias <69852934+Ruan-Dieg0@users.noreply.github.com>
2022-10-20 00:53:03 +0000

    Adicionei e exibi as strings "nome" e "idade"

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 ''' 
 Meu primeiro projeto Python :D
+
 print() = Comando de saida
+Para guardar uma string (Frase): nome = "Ruan Diego" ou idade = 19
+Para exibir string nome/idade: print(nome) ou print(idade)
 
 '''
-print("Hello World")
 
-
+print ("Hello World")
+nome = "Ruan Diego"
+idade = 19
+print (nome, idade)
 
 
 
